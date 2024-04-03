472df9bc1e129e115670dd74a567bf7a7f530185
Klnishant <nishantkaushal075@gmail.com>
2024-04-03 22:45:05 +0530

    basic installation for todo App with redux

---
+++ TodoRedux/src/App.jsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1 className='bg-gray-800 text-white'>Create Todo With Redux</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
